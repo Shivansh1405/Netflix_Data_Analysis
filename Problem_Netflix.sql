7be59e9a856d0df27c34bd90fe6397a018509f0f
Shivansh1405 <149726483+Shivansh1405@users.noreply.github.com>
2025-06-05 13:29:11 +0530

    Add files via upload

---
+++ Problem_Netflix.sql
@@ -0,0 +1,26 @@
+-- 15 Business Problems & Solutions
+
+1.Count the Number of Movies vs TV Shows
+2.Find the Earliest and Latest Release Year List All Movies Released in a Specific Year (e.g., 2020)
+3.Find the Most Common Rating on Netflix
+4.List the Top 5 Most Common Genres
+5.Find the Total Number of TV Shows Released After 2015
+6.Get a List of Unique Countries in the Dataset
+7.Count the Number of Movies and Shows per Year
+8.Retrieve All Content Where Cast Includes 'Leonardo DiCaprio'
+9. All Movies Released Between 2010 and 2020
+10.List All Movies that are Documentaries
+11.Find the Top 5 Directors with the Most Movies on Netflix
+12. Find How Many Movies Actor 'Salman Khan' Appeared in the Last 10 Years
+13.Find All Content That Does Not Have a Director Listed
+14.Find the Number of Movies Available in Each Country
+Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
+
+
+
+
+
+
+
